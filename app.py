c8ba60d4d96e1c8ad069dbd1a6ae61cb074c525a
Porsha <78366933+quistty@users.noreply.github.com>
2023-08-21 17:21:52 -0400

    finding lengths of phrases

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
 phrase = "heyo whats good bruv"
-print(phrase.isupper()) #isupper is will respond true or false to see if it is fully uppercase or not, upper to make a phrase upper case (same for lowercase)
+print(len(phrase)) #checks length of phrase
