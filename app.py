9e5329ad93f6c785335b79299851113e623b603e
Porsha <78366933+quistty@users.noreply.github.com>
2023-08-21 17:50:29 -0400

    converting integers to strings to print to console

--- app.py
+++ app.py
@@ -1,5 +1,2 @@
-david_money = "427493274729384"
-print(2.47386432648723843 + -1.4327264768723 * 75) #python will use bedmas
-print(david_money + "is how much money david has") #use brackets to make python priortize somethings
-print(10 % 3) #use this division to return the remainder
-
+david_money = 427493274729384
+print(str(david_money) + " is my favourite number") #if you want to print an integer you must convert it to a string first
