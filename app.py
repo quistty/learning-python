e3f1e5b398673536c67db6686f8152cc989da613
Porsha <78366933+quistty@users.noreply.github.com>
2023-08-21 17:48:32 -0400

    just general number use

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-phrase = "heyo whats good bruv"
-        #0123456789...
-print(phrase.replace("heyo", "blud")) #replace function, straight forward
-
+david_money = "427493274729384"
+print(2.47386432648723843 + -1.4327264768723 * 75) #python will use bedmas
+print(david_money + "is how much money david has") #use brackets to make python priortize somethings
+print(10 % 3) #use this division to return the remainder
 
