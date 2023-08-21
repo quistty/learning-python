a9589406a398f9502b6728b90c89ef4a1efe3c77
Porsha <78366933+quistty@users.noreply.github.com>
2023-08-21 19:23:15 -0400

    creating a basic calculator along with a madlib

---
+++ using strings/madlib.py
@@ -0,0 +1,6 @@
+colour = input("enter a colour: ")
+plr_noun = input("enter a plural noun: ")
+celebrity = input("enter a celebrity: ")
+print("roses are " + colour)
+print(plr_noun + " are blue")
+print("I love " + celebrity)
