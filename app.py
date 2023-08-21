281fec88de0a3f87e8712fafe4c00fea52e40eff
Porsha <78366933+quistty@users.noreply.github.com>
2023-08-21 18:11:27 -0400

    some more math functions

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-david_money = 427493274729384
-print(str(david_money) + " is my favourite number") #if you want to print an integer you must convert it to a string first
+david_money = -427493274729384.43274632842
+yall = 4728473
+print(abs(david_money))  # printing absolute number
+print(pow(5, 3))  # raising a number to a certain power
+print(max(david_money, yall))  # tells me the biggest number
+print(min(david_money, yall))  # tells me the smallest number
+print(round(david_money))  # rounds (would round 3.7 to a whole number)
+david_money = 43235
