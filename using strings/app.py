ab914133d1c34bfca7f48a2319a45cfa63b3b6a8
Porsha <78366933+quistty@users.noreply.github.com>
2023-08-21 19:15:41 -0400

    more important history

--- using strings/app.py
+++ using strings/app.py
@@ -1,10 +1,1 @@
-# input is by default making this a string
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-
-# this converts the string from a number to an integer
-# you can change this to a float (which allows for decimals)
-result = int(num1) + int(num2)
-
-
-print(result)
+# see history of this file
