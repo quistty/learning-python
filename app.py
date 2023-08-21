3aa4d310cd0e4e2d8c29b9121bef3af7f0ba1289
Porsha <78366933+quistty@users.noreply.github.com>
2023-08-21 17:38:22 -0400

    #replace function, straight forward

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 phrase = "heyo whats good bruv"
         #0123456789...
-print(phrase.index("G")) #this is passing a parameter, value that i would give to a function
+print(phrase.replace("heyo", "blud")) #replace function, straight forward
 
 
