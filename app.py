534a95937f46556da66fd7d7659a6d087e42b264
Porsha <78366933+quistty@users.noreply.github.com>
2023-08-21 18:15:52 -0400

    importing math module

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-david_money = -427493274729384.43274632842
-yall = 4728473
-print(abs(david_money))  # printing absolute number
-print(pow(5, 3))  # raising a number to a certain power
-print(max(david_money, yall))  # tells me the biggest number
-print(min(david_money, yall))  # tells me the smallest number
-print(round(david_money))  # rounds (would round 3.7 to a whole number)
-david_money = 43235
+from math import *  # importing commands from math library KNOWN AS A MODULE IN PYTHON
+
+print(floor(9.942732421))  # rounds down ALWAYS
+print(ceil(5.4324372423))  # rounds up ALWAYS
+numba = 36
+print(str(sqrt(numba)) + " is the square root of " + str(numba))
