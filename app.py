f89fb3b3303ca4daccba2d24a2703c6bd6b67243
Porsha <78366933+quistty@users.noreply.github.com>
2023-08-21 17:19:09 -0400

    upper, lower, isupper and islower

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
-phrase = "heyo"
-print(phrase + " ca va?") #concatenation, the process of taking a string and appending another string onto it
+phrase = "heyo whats good bruv"
+print(phrase.isupper()) #isupper is will respond true or false to see if it is fully uppercase or not, upper to make a phrase upper case (same for lowercase)
