8e0ddb77315d122b110c9c3dbf61120c7c49d250
Porsha <78366933+quistty@users.noreply.github.com>
2023-08-20 20:44:27 -0400

    testing

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 character_Name = "John"
 character_name2 = "David"
 character_age = 50
-isMale = True # this is a boolean, the value has to be capitalized and must be true or false obv
+isMale = True # this is a boolean, the value has to be capitalized and must be "True" or "False" obv
 print("There once was a man named " + character_Name + " who loved life at " + character_age + " years old but didn't like being " + character_age + " years old so he changed his name from " + character_Name + " to " + character_name2)
